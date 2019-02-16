45b410ed076f034936d375cced2f77346e0ce737
Rohit Yadav <rohit@yadav.cloud>
2019-02-16 21:36:49 +0530

    old: old styled bash based env deployment
    Signed-off-by: Rohit Yadav <rohit@yadav.cloud>

---
+++ old/deploy-xen-adv.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+# Recipe to deploy an advanced zone
+# By Rohit Yadav <rohit@scaleninja.com>
+set -e
+set -x
+
+cli=cloudmonkey
+dns_ext=8.8.8.8
+dns_int=192.168.1.1
+gw=192.168.1.1
+nmask=255.255.0.0
+hpvr=XenServer
+pod_start=192.168.51.10
+pod_end=192.168.51.250
+vlan_start=192.168.50.10
+vlan_end=192.168.50.250
+
+#Put space separated host ips in following
+host_ips="192.168.3.113 192.168.3.234"
+host_user=root
+host_passwd=password
+sec_storage='nfs://192.168.1.11/export/testing/secondary-master'
+prm_storage='nfs://192.168.1.11/export/testing/primary-master'
+local_storage=true
+
+$cli set profile local
+$cli set username admin
+$cli set password password
+$cli set display default
+$cli set asyncblock true
+
+zone_id=`$cli create zone dns1=$dns_ext internaldns1=$dns_int localstorageenabled=$local_storage name=MyZone networktype=Advanced securitygroupenabled=false guestcidraddress=10.1.1.0/24 | grep ^id\ = | awk '{print $3}'`
+echo "Created zone" $zone_id
+
+phy_id=`$cli create physicalnetwork name=phy-network zoneid=$zone_id isolationmethods=VLAN | grep ^id\ = | awk '{print $3}'`
+echo "Created physical network" $phy_id
+$cli add traffictype traffictype=Management physicalnetworkid=$phy_id
+echo "Added mgmt traffic"
+$cli add traffictype traffictype=Public physicalnetworkid=$phy_id
+echo "Added mgmt traffic"
+$cli add traffictype traffictype=Guest physicalnetworkid=$phy_id
+echo "Added guest traffic"
+
+$cli update physicalnetwork state=Enabled id=$phy_id
+echo "Enabled physicalnetwork"
+
+nsp_id=`$cli list networkserviceproviders name=VirtualRouter physicalnetworkid=$phy_id | grep ^id\ = | awk '{print $3}'`
+vre_id=`$cli list virtualrouterelements nspid=$nsp_id | grep ^id\ = | awk '{print $3}'`
+$cli configure virtualrouterelement enabled=true id=$vre_id
+$cli update networkserviceprovider state=Enabled id=$nsp_id
+echo "Enabled virtual router element and network service provider"
+
+nsp_id=`$cli list networkserviceproviders name=Internallbvm physicalnetworkid=$phy_id | grep ^id\ = | awk '{print $3}'`
+ilbvm_id=`$cli list internalloadbalancerelements nspid=$nsp_id | grep ^id\ = | awk '{print $3}'`
+$cli configure internalloadbalancerelement enabled=true id=$ilbvm_id
+$cli update networkserviceprovider state=Enabled id=$nsp_id
+echo "Enabled Internal LBVM and NSP"
+
+nsp_id=`$cli list networkserviceproviders name=VpcVirtualRouter physicalnetworkid=$phy_id | grep ^id\ = | awk '{print $3}'`
+vpcvr_id=`$cli list virtualrouterelements nspid=$nsp_id | grep ^id\ = | awk '{print $3}'`
+$cli configure virtualrouterelement enabled=true id=$vpcvr_id
+$cli update networkserviceprovider state=Enabled id=$nsp_id
+echo "Enabled VPC VR and NSP"
+
+pod_id=`$cli create pod name=MyPod zoneid=$zone_id gateway=$gw netmask=$nmask startip=$pod_start endip=$pod_end | grep ^id\ = | awk '{print $3}'`
+echo "Created pod"
+
+$cli create vlaniprange zoneid=$zone_id vlan=untagged gateway=$gw netmask=$nmask startip=$vlan_start endip=$vlan_end forvirtualnetwork=true
+echo "Created IP ranges for instances"
+
+$cli update physicalnetwork vlan=700-900 id=$phy_id
+echo "Update physical network"
+
+cluster_id=`$cli add cluster zoneid=$zone_id hypervisor=$hpvr clustertype=CloudManaged podid=$pod_id clustername=MyCluster | grep ^id\ = | awk '{print $3}'`
+echo "Created cluster" $cluster_id
+
+#Put loop here if more than one
+for host_ip in $host_ips;
+do
+  $cli add host zoneid=$zone_id podid=$pod_id clusterid=$cluster_id hypervisor=$hpvr clustertype=CloudManaged username=$host_user password=$host_passwd url=http://$host_ip;
+  echo "Added host" $host_ip;
+done;
+
+$cli create storagepool zoneid=$zone_id podid=$pod_id clusterid=$cluster_id name=MyNFSPrimary hypervisor=$hpvr scope=zone url=$prm_storage
+echo "Added primary storage"
+
+$cli add imagestore name=MyNFSSecondary provider=NFS zoneid=$zone_id url=$sec_storage
+echo "Added secondary storage"
+
+# general global settings
+$cli update zone allocationstate=Enabled id=$zone_id
+echo "Basic zone deloyment completed!"
---
+++ old/deploy-xen-basicsg.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+set -e
+set -x
+
+cli=cloudmonkey
+dns_ext=8.8.8.8
+dns_int=192.168.1.1
+gw=192.168.1.1
+nmask=255.255.0.0
+hpvr=XenServer
+pod_start=192.168.51.10
+pod_end=192.168.51.250
+vlan_start=192.168.50.10
+vlan_end=192.168.50.250
+
+#Put space separated host ips in following
+host_ips="192.168.3.113 192.168.3.234"
+host_user=root
+host_passwd=password
+sec_storage='nfs://192.168.1.11/export/testing/secondary-master'
+prm_storage='nfs://192.168.1.11/export/testing/primary-master'
+local_storage=false
+enable_sg=false
+
+$cli set profile local
+$cli set username admin
+$cli set password password
+$cli set display default
+$cli set asyncblock true
+
+zone_id=`$cli create zone dns1=$dns_ext internaldns1=$dns_int localstorageenabled=$local_storage securitygroupenabled=$enable_sg name=MyZone networktype=Basic | grep ^id\ = | awk '{print $3}'`
+echo "Created zone" $zone_id
+
+phy_id=`$cli create physicalnetwork name=phy-network zoneid=$zone_id | grep ^id\ = | awk '{print $3}'`
+echo "Created physical network" $phy_id
+$cli add traffictype traffictype=Guest physicalnetworkid=$phy_id
+echo "Added guest traffic"
+$cli add traffictype traffictype=Management physicalnetworkid=$phy_id
+echo "Added mgmt traffic"
+$cli update physicalnetwork state=Enabled id=$phy_id
+echo "Enabled physicalnetwork"
+
+nsp_id=`$cli list networkserviceproviders name=VirtualRouter physicalnetworkid=$phy_id | grep ^id\ = | awk '{print $3}'`
+vre_id=`$cli list virtualrouterelements nspid=$nsp_id | grep ^id\ = | awk '{print $3}'`
+$cli api configureVirtualRouterElement enabled=true id=$vre_id
+$cli update networkserviceprovider state=Enabled id=$nsp_id
+echo "Enabled virtual router element and network service provider"
+
+nsp_sg_id=`$cli list networkserviceproviders name=SecurityGroupProvider physicalnetworkid=$phy_id | grep ^id\ = | awk '{print $3}'`
+$cli update networkserviceprovider state=Enabled id=$nsp_sg_id
+echo "Enabled security group provider"
+
+#netoff_id=$($cli list networkofferings name=DefaultSharedNetworkOfferingWithSGService guestiptype=Shared state=Enabled | grep ^id\ = | awk '{print $3}')
+#net_id=`$cli create network zoneid=$zone_id name=guestNetworkForBasicZone displaytext=guestNetworkForBasicZone networkofferingid=$netoff_id | grep ^id\ = | awk '{print $3}'`
+#echo "Created network $net_id for zone" $zone_id
+
+netoff_id=$($cli list networkofferings name=DefaultSharedNetworkOffering guestiptype=Shared state=Enabled  displaytext="Offering for Shared networks" | grep ^id\ = | awk '{print $3}')
+net_id=`$cli create network zoneid=$zone_id name=guestNetworkForBasicZone displaytext=guestNetworkForBasicZone networkofferingid=$netoff_id | grep ^id\ = | awk '{print $3}'`
+echo "Created network $net_id for zone" $zone_id
+
+pod_id=`$cli create pod name=MyPod zoneid=$zone_id gateway=$gw netmask=$nmask startip=$pod_start endip=$pod_end | grep ^id\ = | awk '{print $3}'`
+echo "Created pod"
+
+$cli create vlaniprange podid=$pod_id networkid=$net_id gateway=$gw netmask=$nmask startip=$vlan_start endip=$vlan_end forvirtualnetwork=false
+echo "Created IP ranges for instances"
+
+cluster_id=`$cli add cluster zoneid=$zone_id hypervisor=$hpvr clustertype=CloudManaged podid=$pod_id clustername=MyCluster | grep ^id\ = | awk '{print $3}'`
+echo "Created cluster" $cluster_id
+
+#Put loop here if more than one
+for host_ip in $host_ips;
+do
+  $cli add host zoneid=$zone_id podid=$pod_id clusterid=$cluster_id hypervisor=$hpvr username=$host_user password=$host_passwd url=http://$host_ip;
+  echo "Added host" $host_ip;
+done;
+
+$cli create storagepool zoneid=$zone_id podid=$pod_id clusterid=$cluster_id name=MyNFSPrimary url=$prm_storage
+echo "Added primary storage"
+
+$cli add secondarystorage zoneid=$zone_id url=$sec_storage
+echo "Added secondary storage"
+
+$cli update zone allocationstate=Enabled id=$zone_id
+echo "Basic zone deloyment completed!"
